0f81e5eb0e832f1bbcac07737e284d07a302667e
MegaThorx <git@merx.dev>
2021-05-15 13:03:15 +0200

    Addendum

---
+++ index.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const fs = require('fs');
+const path = require('path');
+
+try {
+    const folder = core.getInput('folder');
+    const secret = core.getInput('secret');
+
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+
+    console.log(__dirname);
+} catch (error) {
+    core.setFailed(error.message);
+}
